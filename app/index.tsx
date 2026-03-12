1f2f5abb26b9a6652dd8e9df2e2c983c42fb1ce3
Arthur5502 <arthurfelipecampos@hotmail.com>
2026-03-12 20:37:51 -0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import News from "../components/News";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <News />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
